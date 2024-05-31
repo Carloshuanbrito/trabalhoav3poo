cb376ce5f4e51d00feb089ce6dd4b2289e53afa5
Carloshuanbrito <144080061+Carloshuanbrito@users.noreply.github.com>
2024-05-31 17:42:13 -0300

    Add files via upload

---
+++ trabalhoav3/src/app/Hospede.java
@@ -0,0 +1,35 @@
+package app;
+
+import java.util.ArrayList;
+
+public class Hospede extends Pessoa {
+	private String enderecoCompleto;
+	
+	
+
+	public String getEnderecoCompleto() {
+		return enderecoCompleto;
+	}
+
+	public void setEnderecoCompleto(String enderecoCompleto) {
+		this.enderecoCompleto = enderecoCompleto;
+	}
+	
+	public boolean cadastrar(Hospede hospede) {
+        return true;
+    }
+
+    public boolean editar(Hospede hospede) {
+        return true;
+    }
+
+    public Hospede consultar(Hospede hospede) {
+
+        return hospede;
+    }
+
+    public ArrayList<Hospede> listar() {
+        return new ArrayList<Hospede>();
+    }
+	
+}
